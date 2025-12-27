2923a9f1701682535ef8f0e1b95160a65fa63269
Abdelrahman Fathy <abdofathy883@gmail.com>
2025-12-27 05:23:47 +0200

    fixed dependencies versions

---
+++ Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PortfolioDbContext>
+    {
+        public PortfolioDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory();
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnectionString");
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found in configuration sources.");
+
+            var builder = new DbContextOptionsBuilder<PortfolioDbContext>();
+            builder.UseSqlServer(connectionString);
+
+            return new PortfolioDbContext(builder.Options);
+        }
+    }
+}
